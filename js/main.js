28d567f9872320127a67d8bf1356c6ba8edbd0ec
Rodrigo Novaes <rod_novaes@hotmail.com>
2019-10-22 17:17:47 +1300

    Add Sliders section

--- js/main.js
+++ js/main.js
@@ -9,3 +9,28 @@ $('.slider-one')
     prevArrow: '.site-slider .slider-btn .prev',
     nextArrow: '.site-slider .slider-btn .next'
   });
+
+// Second Slider
+
+$('.slider-two')
+  .not('.slick-initialized')
+  .slick({
+    prevArrow: '.site-slider-two .prev',
+    nextArrow: '.site-slider-two .next',
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000
+  });
+
+// Third Slider
+
+$('.slider-three')
+  .not('.slick-initialized')
+  .slick({
+    prevArrow: '.site-slider-three .prev',
+    nextArrow: '.site-slider-three .next',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+    inifinite: false
+  });
